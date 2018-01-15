e71d58604d0298afb91c4fd20b441b90cf302e5d
Unknown <anneclaire.vergote@gmail.com>
2018-01-15 09:17:24 +0100

    Ajout des action supprimer et enregistrer

---
+++ src/gestionprojet/controleur/actions/ActionEnregistrer.java
@@ -0,0 +1,37 @@
+package gestionprojet.controleur.actions;
+
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import javax.swing.AbstractAction;
+import javax.swing.JFileChooser;
+
+
+public class ActionEnregistrer extends AbstractAction{
+
+	private static final long serialVersionUID =1L;
+	public static final String NOM_ACTION = "Enregistrer";
+	
+	public ActionEnregistrer(){
+		super(NOM_ACTION);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		String action = e.getActionCommand();
+		if(action.equals(NOM_ACTION)){
+			this.enregistrerCalendrier();
+		}
+	}
+	
+	@Override
+	public void enregistrerCalendrier(){
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
---
+++ src/gestionprojet/controleur/actions/ActionSupprimer.java
@@ -0,0 +1,22 @@
+package gestionprojet.controleur.actions;
+
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+
+public class ActionSupprimer extends AbstractAction{
+	
+	private static final long serialVersionUID = 1L;
+	public static final String NOM_ACTION = "Supprimer";
+	
+	public ActionSupprimer(){
+		super(NOM_ACTION);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		String action = e.getActionCommand();
+		
+	}
+	
+	
+}
