8f51a711b2913224781a0fe72c2c76c5eb286981
Unknown <celine.merand@reseau.eseo.fr>
2018-01-17 10:24:53 +0100

    Affichage tableau avec dates en vrac

--- src/gestionprojet/modele/Calendrier.java
+++ src/gestionprojet/modele/Calendrier.java
@@ -1,7 +1,10 @@
 package gestionprojet.modele;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 /*
  * Par Céline MERAND
@@ -14,13 +17,40 @@ public class Calendrier {
 	//Attributs
 	//TODO voir frameworks pour ajouter widget calendrier
 	private Collection<Lot> lot = new ArrayList<>();
+	//private Calendar calendar = new GregorianCalendar();
 	private Date startDate;
 	private Date endDate;
 	
+
 	//Constructeur
+	//TODO vérifier si startDate < endDate
 	public Calendrier(Date startDate, Date endDate) {
-		this.startDate = startDate;
-		this.endDate = endDate;
+		if (endDate.after(startDate)) {
+			this.startDate = startDate;
+			this.endDate = endDate;				
+		}else{
+			this.startDate = endDate;
+			this.endDate = startDate;
+		}
+	}
+	
+	public Calendrier(String startDate, String endDate) {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd");
+			
+			try{
+				Date dd = sdf.parse(startDate);
+				Date df = sdf.parse(endDate);
+				
+				if (df.after(dd)) {
+					this.startDate = dd;
+					this.endDate = df;
+				} else {
+					this.startDate = df;
+					this.endDate = dd;
+				}
+			} catch(Exception ex) {
+				throw new IllegalArgumentException("Mauvais format !!");
+			}
 	}
 
 	//-----------Accesseurs------------
@@ -37,6 +67,11 @@ public class Calendrier {
 		return this.endDate;
 	}
 	
+	public int getDurationInDays() {
+		long diff = this.getEndDate().getTime() - this.getStartDate().getTime();
+		diff = diff/(1000 * 60 * 60 * 24) +1;
+		return (int)diff;
+	}
 	//-----------Mutateurs------------
 	public void setStartDate(Date startDate){
 		this.startDate = startDate;
@@ -47,4 +82,30 @@ public class Calendrier {
 	}
 	
 	//Méthodes
+	
+	//Source : "http://www.baeldung.com/java-between-dates"
+	//Renvoie la liste des dates entre le début et la fin du projet 
+	//TODO ajouter méthode split pour voir un nom propre par colonne
+	public ArrayList<String> getDatesBetween(){
+		ArrayList<String> datesInRange = new ArrayList<>();
+		
+		Calendar calendar = new GregorianCalendar();
+		calendar.setTime(this.getStartDate());
+		
+		Calendar endCalendar = new GregorianCalendar();
+		endCalendar.setTime(this.getEndDate());
+		
+		while(calendar.before(endCalendar)){
+			
+			String result = calendar.getTime().toString();
+			datesInRange.add(result);
+			calendar.add(Calendar.DATE, 1);
+		}
+		datesInRange.add(this.getEndDate().toString());
+		
+		return datesInRange;
+		
+		
+	}
+
 }
--- src/gestionprojet/modele/Lot.java
+++ src/gestionprojet/modele/Lot.java
@@ -1,4 +1,5 @@
 package gestionprojet.modele;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -13,21 +14,38 @@ public class Lot {
 	//TODO : liste de lots
 	private Date startDate;
 	private Date endDate;
-	private int duration;
 	private String name;
 	//TODO ordonner la liste en fonction des dates de début et fin de chaque lot
 	private ArrayList<Personne> personList = new ArrayList<Personne>();
 	private ArrayList<Tache> tasks = new ArrayList<Tache>();
 		
 	//Constructeur
-	public Lot(String name, Date startDate, int duration) {
+	public Lot(String name, Date startDate, Date endDate) {
 		this.name = name;
 		this.startDate = startDate;
-		this.duration = duration;
+		this.endDate = endDate;
 	}
 	
-	public Lot(String name, Date startDate, Date endDate){
-		this(name, startDate, (int)getDateDiff(startDate, endDate));
+	public Lot(String name, String startDate, String endDate){
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd");
+		
+		try{
+			Date dd = sdf.parse(startDate);
+			Date df = sdf.parse(endDate);
+			
+			if (df.after(dd)) {
+				this.name = name;
+				this.startDate = dd;
+				this.endDate = df;
+			} else {
+				this.name = name;
+				this.startDate = df;
+				this.endDate = dd;
+			}
+		} catch(Exception ex) {
+			throw new IllegalArgumentException("Mauvais format !!");
+		}
+		
 	}
 	
 	//-----------Accesseurs------------
@@ -39,8 +57,10 @@ public class Lot {
 		return this.endDate;
 	}
 	
-	public int getDuration(){
-		return this.duration;
+	public int getDurationInDays() {
+		long diff = this.getEndDate().getTime() - this.getStartDate().getTime();
+		diff = diff/(1000 * 60 * 60 * 24) +1;
+		return (int)diff;
 	}
 	
 	public String getName(){
@@ -65,9 +85,6 @@ public class Lot {
 		this.endDate = endDate;
 	}
 	
-	public void setDuration(int duration){
-		this.duration = duration;
-	}
 	
 	public void setName(String name){
 		this.name = name;
@@ -82,8 +99,5 @@ public class Lot {
 	}
 	
 	//Méthodes
-	public static long getDateDiff(Date date1, Date date2) {
-	    long diffInMillies = date2.getTime() - date1.getTime();
-	    return diffInMillies/(1000*3600*24);
-	}
+	
 }
